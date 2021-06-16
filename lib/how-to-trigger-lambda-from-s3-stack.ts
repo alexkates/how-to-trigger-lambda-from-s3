20a39e9bb9704a31bee731dc4c454f16793c03ce
Daniel Alexander Kates <alex.kates@creditly.com>
2021-06-16 08:43:46 -0400

    added bucket

--- lib/how-to-trigger-lambda-from-s3-stack.ts
+++ lib/how-to-trigger-lambda-from-s3-stack.ts
@@ -1,9 +1,16 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
 
 export class HowToTriggerLambdaFromS3Stack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, 'OurBucket', {
+      /**
+       * The following properties ensure the bucket is properly 
+       * deleted when we run cdk destroy */
+      autoDeleteObjects: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    })
   }
 }
