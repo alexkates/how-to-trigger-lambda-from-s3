9767dcabed442589838f58970bdad345c8e8ae46
Daniel Alexander Kates <alex.kates@creditly.com>
2021-06-16 16:14:18 -0400

    finished post

--- lib/how-to-trigger-lambda-from-s3-stack.ts
+++ lib/how-to-trigger-lambda-from-s3-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as lambdaEventSources from '@aws-cdk/aws-lambda-event-sources';
 
 export class HowToTriggerLambdaFromS3Stack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -11,6 +13,21 @@ export class HowToTriggerLambdaFromS3Stack extends cdk.Stack {
        * deleted when we run cdk destroy */
       autoDeleteObjects: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY
-    })
+    });
+
+    const lambdaFunction = new lambda.Function(this, 'Function', {
+      code: lambda.Code.fromAsset('src'),
+      handler: 'index.handler',
+      functionName: 'BucketPutHandler',
+      runtime: lambda.Runtime.NODEJS_12_X,
+    });
+
+    const s3PutEventSource = new lambdaEventSources.S3EventSource(bucket, {
+      events: [
+        s3.EventType.OBJECT_CREATED_PUT
+      ]
+    });
+
+    lambdaFunction.addEventSource(s3PutEventSource);
   }
 }
